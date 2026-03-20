54552ba34bcb1d408a0a970ee7c360925bc68070
Haizhou Ge <1352674740@qq.com>
2026-03-20 20:42:52 +0800

    add dr

--- airdc/managers/auto_atom.py
+++ airdc/managers/auto_atom.py
@@ -28,8 +28,23 @@ class AutoAtomManager(DemonstrateManagerBasis):
             self._runner = TaskRunner().from_config(self.config)
         runner = self._runner
         if state is State.active:
+            """
+            1. runner.reset()
+            └─ MujocoTaskBackend.reset()
+                ├─ env.reset()       ← 重置到 XML 默认值
+                ├─ operator.home()
+                └─ _apply_randomization()  ✓ 随机化已应用
+
+            2. fsm.act(DAction.sample)
+            └─ PREPARE_EVENT_BEFORE 回调: demonstrator.react(sample)
+                └─ SingleComponentDemonstrator.switch_mode(SAMPLING)
+                    └─ MujocoEnv.on_switch_mode(SAMPLING)
+                        └─ env.reset()   ← !! 随机化被覆盖 !!
+                        # 因为 SAMPLING != RESETTING，所以会 reset
+            """
+            result = fsm.act(DAction.sample)
             runner.reset()
-            return fsm.act(DAction.sample)
+            return result
         elif state is State.sampling:
             result = runner.update()
             if result.done:
