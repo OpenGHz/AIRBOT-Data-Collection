fb0c630dfd2997a0dbdd5d2bfb900e1ea74ece96
Haizhou Ge <1352674740@qq.com>
2026-04-06 10:40:57 +0800

    feat: Added automated atomic operation installation scripts to simplify dependency management

---
+++ install/install_auto_atom.sh
@@ -0,0 +1,10 @@
+set -ex
+
+pip install -e ."[assis]"
+mkdir -p third_party
+git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git third_party/
+
+ln -s $PWD/third_party/auto-atomic-operation/aao_configs airbot_ie/configs/managers/auto_atom/
+
+# bash install/install_auto_atom.sh
+# TASK_NAME=pick_and_place airdc --name aao_config dataset.directory=aao_data/$TASK_NAME managers=auto_atom/$TASK_NAME
