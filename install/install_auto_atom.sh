72b57781dfeaf1764df1fe402fdea0a53876bd6d
Haizhou Ge <1352674740@qq.com>
2026-04-07 15:54:21 +0800

    feat: Update auto-atomic-operation to version 0.2.3 and clean up dependencies in configuration files

--- install/install_auto_atom.sh
+++ install/install_auto_atom.sh
@@ -2,9 +2,8 @@ set -ex
 
 pip install -e ."[assis]"
 mkdir -p third_party
-git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git -b 0.2.2 third_party/
+git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git -b 0.2.3 third_party/
 
 ln -s $PWD/third_party/auto-atomic-operation/aao_configs airbot_ie/configs/managers/auto_atom/
 
-# bash install/install_auto_atom.sh
-# TASK_NAME=pick_and_place airdc --name aao_config dataset.directory=aao_data/$TASK_NAME managers=auto_atom/$TASK_NAME
+# TASK_NAME=pick_and_place airdc --name aao_config dataset.directory=aao_data/$TASK_NAME managers/auto_atom/task=$TASK_NAME
