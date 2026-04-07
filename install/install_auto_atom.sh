3b84fa840ae8ea46533bfb02289a8f4bb9a96f36
Haizhou Ge <1352674740@qq.com>
2026-04-07 16:50:15 +0800

    feat: Update install_auto_atom.sh to clone auto-atomic-operation into a specific directory

--- install/install_auto_atom.sh
+++ install/install_auto_atom.sh
@@ -1,9 +1,6 @@
 set -ex
 
-pip install -e ."[assis]"
 mkdir -p third_party
-git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git -b 0.2.3 third_party/
+git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git -b 0.2.3 third_party/auto-atomic-operation
 
 ln -s $PWD/third_party/auto-atomic-operation/aao_configs airbot_ie/configs/managers/auto_atom/
-
-# TASK_NAME=pick_and_place airdc --name aao_config dataset.directory=aao_data/$TASK_NAME managers/auto_atom/task=$TASK_NAME
