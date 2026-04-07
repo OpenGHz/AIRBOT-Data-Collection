f40db64b418b19158d420ad384f79e1bb6b855a3
Haizhou Ge <1352674740@qq.com>
2026-04-07 17:27:04 +0800

    feat: Update install_auto_atom.sh to clone auto-atomic-operation version 0.2.4 and bump project version to 0.7.1

--- install/install_auto_atom.sh
+++ install/install_auto_atom.sh
@@ -1,6 +1,6 @@
 set -ex
 
 mkdir -p third_party
-git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git -b 0.2.3 third_party/auto-atomic-operation
+git clone --depth 1 https://github.com/OpenGHz/auto-atomic-operation.git -b 0.2.4 third_party/auto-atomic-operation
 
 ln -s $PWD/third_party/auto-atomic-operation/aao_configs airbot_ie/configs/managers/auto_atom/
