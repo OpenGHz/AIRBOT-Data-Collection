3b22937dbfbbe25850ff330f57a1f3a617798ce6
Haizhou Ge <1352674740@qq.com>
2026-04-15 20:47:07 +0800

    feat: Update dependencies and add hydra joblib launcher support

--- airdc/main.py
+++ airdc/main.py
@@ -12,135 +12,143 @@ from airdc.state_machine.fsm import (
 )
 from airdc.basis import PACKAGE_NAME
 from mcap_data_loader.configurers.basis import main_argparse
+from typing import Optional
+
+
+logger = getLogger(PACKAGE_NAME)
+
+
+def main_loop(config: DataCollectionArgs, job_id: Optional[int] = None) -> int:
+    """
+    The main manager of data collection.
+    """
+    main_name = f"{PACKAGE_NAME}[{job_id}]" if job_id is not None else PACKAGE_NAME
+    setproctitle(main_name)
+    logger = getLogger(main_name)
+    logger.info(f"Version: {version(PACKAGE_NAME)}")
+
+    batch_size = config.batch_size
+    fsm_cnt = 0
+    raw_directory = config.dataset.directory
+    # NOTE: Currently, pydantic's model_copy update is always performed after the copy is complete. Therefore, it is not possible to avoid copying issues by clearing fields in the update. The only solution is to manually clear them beforehand.
+    managers = config.managers.copy()
+    config.managers.clear()
+
+    def create_fsm():
+        nonlocal fsm_cnt
+        fsm_cnt += 1
+        # NOTE: if there are multiple FSMs, we only keep the visualizer for the first one to avoid duplicated visualization
+        if batch_size > 0 and fsm_cnt == 1:
+            object.__setattr__(config.dataset, "directory", f"{raw_directory}_0")
+        if fsm_cnt > 1:
+            config_copy = config.model_copy(
+                update={
+                    "visualizer": None,
+                    "dataset": config.dataset.model_copy(
+                        update={"directory": raw_directory + f"_{fsm_cnt - 1}"},
+                        deep=True,
+                    ),
+                    "managers": {},
+                },
+                deep=True,
+            )
+        else:
+            config_copy = config
+        return DemonstrateFSM(
+            DemonstrateFSMConfig(state_machine=config_copy.fsm, interface=config_copy)
+        )
+
+    logger.info(f"Creating {batch_size} FSMs.")
+    fsms = [create_fsm() for _ in range(batch_size or 1)]
+
+    for name, manager in managers.items():
+        manager.set_fsms(fsms)
+        if not manager.configure():
+            raise RuntimeError(f"Failed to configure manager: {name}.")
+    interval = 1.0 / config.update_rate if config.update_rate > 0 else 0.0
+    logger.info(f"Update rate: {config.update_rate} Hz")
+    # start updating the managers
+    # TODO: use async io to update asynchronously?
+    time_queue = deque(maxlen=20)
+    total_start = time.perf_counter()
+    metrics = defaultdict(dict)
+    try:
+        while True:
+            start_time = time.perf_counter()
+            for name, manager in managers.items():
+                m_start = time.perf_counter()
+                if not manager.update():
+                    logger.warning(f"Failed to update manager: {name}.")
+                metrics["durations"][f"update/manager/{name}"] = (
+                    time.perf_counter() - m_start
+                )
+            if all(fsm.get_state() is DemonstrateState.finalized for fsm in fsms):
+                logger.info("Data collection finished.")
+                break
+            if config.log_metrics >= 0:
+                logger.info(
+                    "\nManager Metrics:\n"
+                    + pformat(dict(metrics))
+                    + "\n"
+                    + "FSM Metrics:\n"
+                    + pformat([dict(fsm.metrics) for fsm in fsms])
+                )
+            cost_time = time.perf_counter() - start_time
+            time_queue.append(cost_time)
+            if interval > 0:
+                sleep_time = interval - cost_time
+                if sleep_time > 0:
+                    time.sleep(sleep_time)
+                elif sleep_time < 0 and config.log_jitter:
+                    logger.warning(
+                        f"The main loop takes too long, timeout {-sleep_time:.4f} s."
+                    )
+    except KeyboardInterrupt:
+        logger.info("Keyboard interrupt received. Exiting...")
+    finally:
+        # shutdown the managers
+        for name, manager in managers.items():
+            logger.info(f"Shutting down manager: {name}.")
+            if not manager.shutdown():
+                logger.error(f"Failed to shutdown: {name}.")
+        # shutdown the FSMs
+        for fsm in fsms:
+            if fsm.get_state() is not DemonstrateState.finalized:
+                logger.info("Shutting down FSM.")
+                fsm.shutdown()
+
+    summary = {"Total time taken": f"{time.perf_counter() - total_start:.4f} s"}
+    if time_queue:
+        avg_time = sum(time_queue) / len(time_queue)
+        summary.update(
+            {
+                "Average update time": f"{avg_time:.4f} s",
+                "Average update freq": f"{1.0 / avg_time:.4f} Hz",
+            }
+        )
+    logger.info("Summary:\n" + pformat(summary))
+    logger.info("Done.")
+    return 0
 
 
 def main() -> int:
     configurer = main_argparse(PACKAGE_NAME)(DataCollectionArgs)
-    configurer.parse()
-
-    logger = getLogger(PACKAGE_NAME)
-
-    setproctitle(PACKAGE_NAME)
-
-    def main_loop(config: DataCollectionArgs) -> int:
-        """
-        The main manager of data collection.
-        """
-        logger.info(f"Version: {version(PACKAGE_NAME)}")
-
-        batch_size = config.batch_size
-        fsm_cnt = 0
-        raw_directory = config.dataset.directory
-        # NOTE: Currently, pydantic's model_copy update is always performed after the copy is complete. Therefore, it is not possible to avoid copying issues by clearing fields in the update. The only solution is to manually clear them beforehand.
-        managers = config.managers.copy()
-        config.managers.clear()
-
-        def create_fsm():
-            nonlocal fsm_cnt
-            fsm_cnt += 1
-            # NOTE: if there are multiple FSMs, we only keep the visualizer for the first one to avoid duplicated visualization
-            if batch_size > 0 and fsm_cnt == 1:
-                object.__setattr__(config.dataset, "directory", f"{raw_directory}_0")
-            if fsm_cnt > 1:
-                config_copy = config.model_copy(
-                    update={
-                        "visualizer": None,
-                        "dataset": config.dataset.model_copy(
-                            update={"directory": raw_directory + f"_{fsm_cnt - 1}"},
-                            deep=True,
-                        ),
-                        "managers": {},
-                    },
-                    deep=True,
-                )
-            else:
-                config_copy = config
-            return DemonstrateFSM(
-                DemonstrateFSMConfig(
-                    state_machine=config_copy.fsm, interface=config_copy
-                )
-            )
-
-        logger.info(f"Creating {batch_size} FSMs.")
-        fsms = [create_fsm() for _ in range(batch_size or 1)]
-
-        for name, manager in managers.items():
-            manager.set_fsms(fsms)
-            if not manager.configure():
-                raise RuntimeError(f"Failed to configure manager: {name}.")
-        interval = 1.0 / config.update_rate if config.update_rate > 0 else 0.0
-        logger.info(f"Update rate: {config.update_rate} Hz")
-        # start updating the managers
-        # TODO: use async io to update asynchronously?
-        time_queue = deque(maxlen=20)
-        total_start = time.perf_counter()
-        metrics = defaultdict(dict)
+    multirun = configurer.parse()
+    if multirun:
+        print("Using multirun mode.")
         try:
-            while True:
-                start_time = time.perf_counter()
-                for name, manager in managers.items():
-                    m_start = time.perf_counter()
-                    if not manager.update():
-                        logger.warning(f"Failed to update manager: {name}.")
-                    metrics["durations"][f"update/manager/{name}"] = (
-                        time.perf_counter() - m_start
-                    )
-                if all(fsm.get_state() is DemonstrateState.finalized for fsm in fsms):
-                    logger.info("Data collection finished.")
-                    break
-                if config.log_metrics >= 0:
-                    logger.info(
-                        "\nManager Metrics:\n"
-                        + pformat(dict(metrics))
-                        + "\n"
-                        + "FSM Metrics:\n"
-                        + pformat([dict(fsm.metrics) for fsm in fsms])
-                    )
-                cost_time = time.perf_counter() - start_time
-                time_queue.append(cost_time)
-                if interval > 0:
-                    sleep_time = interval - cost_time
-                    if sleep_time > 0:
-                        time.sleep(sleep_time)
-                    elif sleep_time < 0 and config.log_jitter:
-                        logger.warning(
-                            f"The main loop takes too long, timeout {-sleep_time:.4f} s."
-                        )
+            return configurer.configure(main=main_loop)
         except KeyboardInterrupt:
-            logger.info("Keyboard interrupt received. Exiting...")
-        finally:
-            # shutdown the managers
-            for name, manager in managers.items():
-                logger.info(f"Shutting down manager: {name}.")
-                if not manager.shutdown():
-                    logger.error(f"Failed to shutdown: {name}.")
-            # shutdown the FSMs
-            for fsm in fsms:
-                if fsm.get_state() is not DemonstrateState.finalized:
-                    logger.info("Shutting down FSM.")
-                    fsm.shutdown()
-
-        summary = {"Total time taken": f"{time.perf_counter() - total_start:.4f} s"}
-        if time_queue:
-            avg_time = sum(time_queue) / len(time_queue)
-            summary.update(
-                {
-                    "Average update time": f"{avg_time:.4f} s",
-                    "Average update freq": f"{1.0 / avg_time:.4f} Hz",
-                }
-            )
-        logger.info("Summary:\n" + pformat(summary))
-        logger.info("Done.")
-        return 0
-
-    return main_loop(configurer.configure())
+            logger.info("Keyboard interrupt received.")
+            return 0
+    else:
+        return main_loop(configurer.configure())
 
 
 def main_desktop() -> int:
     import os
     from pathlib import Path
 
-    logger = getLogger(PACKAGE_NAME)
     work_dir = Path(__file__).parent.parent
     if not (work_dir / "airdc.desktop").exists():
         raise RuntimeError(
